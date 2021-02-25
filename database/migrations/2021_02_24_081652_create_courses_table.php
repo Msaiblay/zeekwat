2b0763379202cb6f4e605ed68e3253b13af3ed04
Msaiblay <babykalay96Q@gmail.com>
2021-02-25 12:00:19 +0630

    template

--- database/migrations/2021_02_24_081652_create_courses_table.php
+++ database/migrations/2021_02_24_081652_create_courses_table.php
@@ -16,15 +16,20 @@ class CreateCoursesTable extends Migration
         Schema::create('courses', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('coverphoto');
             $table->text('description');
             $table->text('requirement');
             $table->text('accessstatus');
             $table->text('price');
             $table->boolean('allowdiscount');
+            $table->boolean('publicstatus');
+
 
             $table->foreignId('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->foreignId('language_id')->references('id')->on('languages')->onDelete('cascade');
 //            $table->foreignId('category_id')->references('id')->on('categories')->onDelete('cascade');
             $table->foreignId('subcategory_id')->references('id')->on('subcategories')->onDelete('cascade');
+
 
             $table->softDeletes();
             $table->timestamps();
