f8ea1e9898762e851d9ce387e1138eff9bec88db
Msaiblay <babykalay96@gmail.com>
2021-03-08 14:11:21 +0630

    msai

---
+++ resources/views/auth/teacherregister.blade.php
@@ -0,0 +1,47 @@
+<x-frontend>
+    <div class="container bg-purple-400">
+        <div class="row">
+            <div class="col-md-12 min-vh-100 d-flex flex-column justify-content-center">
+                <div class="row">
+                    <div class="col-lg-6 col-md-8 mx-auto">
+
+                        <!-- form card login -->
+                        <div class="card rounded shadow shadow-sm">
+                            <div class="card-header">
+                                <h3 class="mb-0">Student Register Form</h3>
+                            </div>
+                            <div class="card-body">
+                                <form class="form" role="form" autocomplete="off" id="formLogin" novalidate="" action="{{route('register')}}" method="POST">
+                                    @csrf
+                                    <div class="form-group">
+                                        <label for="uname1">email</label>
+                                        <input type="text" class="form-control form-control-lg rounded-0" name="email" id="uname1" required="">
+                                        <div class="invalid-feedback">Oops, you missed this one.</div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label>Password</label>
+                                        <input type="password" class="form-control form-control-lg rounded-0" name="password" id="pwd1" required="" autocomplete="new-password">
+                                        <div class="invalid-feedback">Enter your password too!</div>
+                                    </div>
+                                    <button type="submit" class="btn btn-secondary btn-lg float-right" id="btnLogin">Login</button>
+                                </form>
+                            </div>
+                            <!--/card-block-->
+                        </div>
+                        <!-- /form card login -->
+
+                    </div>
+
+
+                </div>
+                <!--/row-->
+
+            </div>
+            <!--/col-->
+        </div>
+        <!--/row-->
+    </div>
+    <!--/container-->
+    <script>
+    </script>
+</x-frontend>
